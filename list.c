3af75e6d6893ca9f5f112961308ca56446b867d8
ShlyonskiyDaniil <daniil.shlyonskiy@phystech.edu>
2018-12-15 00:06:06 +0300

    List using.

--- list.c
+++ list.c
@@ -10,7 +10,7 @@ typedef struct slist{
     list *head;
     list *tail;
 } slist;
-// создание нового списка
+
 slist * new_list(void) {
     slist *lt = malloc(sizeof(slist));
     lt->size = 0;
@@ -18,7 +18,6 @@ slist * new_list(void) {
     lt->tail = lt->head;
     return lt;
 }
-// удаление списка
 void delete_list(list ** head) {
     list* prev = NULL;
     while ((*head)->next) {
@@ -28,28 +27,27 @@ void delete_list(list ** head) {
     }
     free(*head);
 }
-//вставка элемента на n-ое занчение
 void insert(list*head, unsigned n,int value) {
     unsigned i = 0;
     list *tmp = NULL;
-    // Находим нужный элемент, если вышли за пределы списка, то выходим из цикла
+    // Находим нужный элемент, если вышли за пределы списка, то выходим из цикла   
     while (i<n && head->next) {
         head = head->next;
         i++;
     }
     tmp = (list*)malloc(sizeof(list));
     tmp->value = value;
-    // если это не послдений элемент, то next  перекидываем на следущий узел
+    // если єто не послдений элемент, то next  перекидываем на следущий узел
     if (head->next) {
         tmp->next = head->next;
-    }
+    } 
     // иначе на NULL
     else {
         tmp->next = NULL;
     }
     head->next = tmp;
 }
-// печатаем список
+
 void print_list(const struct list* head) {
     while (head) {
         printf("%d",head->value);
@@ -57,6 +55,62 @@ void print_list(const struct list* head) {
     }
     printf("\n");
 }
-int main () {
+int main ()
+{
+    // list* first_list = new_list();
+    // insert(first_list, 2, 7);
+    // print_list(first_list);
+    // Печать происходит без пробелов.
+
+    // list* second_list = new_list();
+    // insert(NULL, 2, 7);
+    // Segmentation fault, если положить NULL в функцию insert.
+
+    // list* third_list = new_list();
+    // insert(third_list, 2, 7);
+    // print_list(NULL);
+    // Ничего не печатает, если положить NULL. Программа завершается без ошибок.
+
+    // list* first_list = new_list();
+    // insert(first_list, -1, 7);
+    // insert(first_list, 9999999, 8);
+    // insert(first_list, 0, 9);
+    // insert(first_list, 1, 5);
+    // print_list(first_list);
+    // Если подавать номер места для вставки неккоректно(номер больше размера списка/отрицаетльное число),
+    // то программа завершается без ошибок, вставка происходит в конец списка.
+    // Но если подавать 0 и 1, то место вставки неочевидно.
+
+    // list* first_list = new_list();
+    // insert(first_list, 2, 7);
+    // insert(first_list, 2, 7);
+    // insert(first_list, 2, 7);
+    // print_list(first_list);
+    // delete_list(&first_list);
+    // print_list(first_list);
+    // При удалении списка последний узел не удалаяется.
+
+    // list* first_list = new_list();
+    // insert(first_list, 2, 7);
+    // print_list(first_list);
+    // delete_list(NULL);
+    // print_list(first_list);
+    // При подаче NULL в delete возникает Segmentation fault.
+
+    // list* first_list = new_list();
+    // insert(first_list, 2, 7);
+    // insert(first_list, 2, 7);
+    // print_list(first_list);
+    // delete_list(3);
+    // При случайной подаче неверных данных в delete возникает Segmentation fault. В
+
+    list* first_list = new_list();
+    insert(first_list, 2, 7);
+    insert(first_list, 2, 8);
+    insert(first_list, 1234, 4);
+    insert(first_list, 0, 3);
+    print_list(first_list);
+    delete_list(&first_list);
+
     return 0;
 }
